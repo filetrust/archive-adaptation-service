9789b601c4f442585614227aa353934b6bc84d98
Matt <matt.dignum@googlemail.com>
2020-11-17 11:34:56 +0000

    Remove unneeded config map

--- pkg/pod.go
+++ pkg/pod.go
@@ -106,16 +106,6 @@ func (pa PodArgs) GetPodObject() *core.Pod {
 						},
 					},
 				},
-				{
-					Name: "request-processing-config",
-					VolumeSource: core.VolumeSource{
-						ConfigMap: &core.ConfigMapVolumeSource{
-							LocalObjectReference: core.LocalObjectReference{
-								Name: "request-processing-config",
-							},
-						},
-					},
-				},
 			},
 			Containers: []core.Container{
 				{
@@ -132,7 +122,6 @@ func (pa PodArgs) GetPodObject() *core.Pod {
 					VolumeMounts: []core.VolumeMount{
 						{Name: "sourcedir", MountPath: pa.InputMount},
 						{Name: "targetdir", MountPath: pa.OutputMount},
-						{Name: "request-processing-config", MountPath: "/app/config"},
 					},
 					Resources: core.ResourceRequirements{
 						Limits: core.ResourceList{
